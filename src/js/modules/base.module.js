4bc5025366b02258b79973fb6d619bc8d84c7f86
du.kulaevskiy <thhwitcher@gmail.com>
2016-03-17 11:58:34 +0300

    es6 classes

--- src/js/modules/base.module.js
+++ src/js/modules/base.module.js
@@ -5,51 +5,53 @@
 'use strict';
 
 
-var Base = function () {
-    this._events = {};
-};
-
-Base.prototype._initEvent = function _initEvent(event) {
-    if (!this._events.hasOwnProperty(event)) {
-        this._events[event] = [];
+class Base {
+    constructor () {
+        this._events = {};
     }
-};
 
-Base.prototype._notify = function _notify(event, ...args) {
-    this._initEvent(event);
-
-    for(let i = 0; i < this._events[event].length; ++i) {
-        let func = this._events[event][i];
-        let copy_args = args.slice();
-
-        // TODO Event object
-        copy_args.unshift({
-            name: event
-        });
-
-        func.apply(this, copy_args);
-    }
-};
-
-Base.prototype.subscribe = function subscribe(event, func) {
-    this._initEvent(event);
-
-    this._events[event].push(func);
-};
-
-Base.prototype.unsubscribe = function unsubscribe(event, func) {
-    this._initEvent(event);
-
-    if (!func) {
-        this._events[event] = [];    
-    } else {
-        for (let i = 0; i < this._events[event].length; ++i) {
-            let f = this._events[event][i];
-            if (f === func) {
-                this._events[event].splice(i, 1);
-            }
+    _initEvent(event) {
+        if (!this._events.hasOwnProperty(event)) {
+            this._events[event] = [];
         }
-    }   
+    }
+
+    _notify(event, ...args) {
+        this._initEvent(event);
+
+        for(let i = 0; i < this._events[event].length; ++i) {
+            let func = this._events[event][i];
+            let copy_args = args.slice();
+
+            // TODO Event object
+            copy_args.unshift({
+                name: event
+            });
+
+            func.apply(this, copy_args);
+        }
+    }
+
+    unsubscribe(event, func) {
+        this._initEvent(event);
+
+        if (!func) {
+            this._events[event] = [];    
+        } else {
+            for (let i = 0; i < this._events[event].length; ++i) {
+                let f = this._events[event][i];
+                if (f === func) {
+                    this._events[event].splice(i, 1);
+                }
+            }
+        }   
+    }
+
+    subscribe(event, func) {
+        this._initEvent(event);
+
+        this._events[event].push(func);
+    }
 };
 
 module.exports = Base;
