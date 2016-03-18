3a587525ac5440f9e17b528b85abe8fbf1d53b8c
aa.shirkin <aa.shirkin@tensor.ru>
2016-03-18 10:11:54 +0300

    remove unused code

--- src/js/main.js
+++ src/js/main.js
@@ -10,17 +10,14 @@ const dialog = remote.dialog;
 const app = remote.app;
 
 const fs = require('fs');
-const path = require('path');
 const dots = require('dot').process({path: './src/views'});
 
-const RECENT_ARCFILE_KEY = '662589eb-a07c-42eb-bac2-ddd9f75f7fc9';
-// const RECENT_WORKSPACE_KEY = 'dae30f50-b34c-4e4c-83e6-fe0eeb2ded50';
+//noinspection JSFileReferences
+const FileDragDrop = require('./js/modules/dragdrop.module');
+//noinspection JSFileReferences
+const Arc = require('./js/modules/Arc.module');
 
-// var dearcer = require('./js/modules/arcfile.module').dearcer;
-//noinspection JSFileReferences
-var FileDragDrop = require('./js/modules/dragdrop.module');
-//noinspection JSFileReferences
-var Arc = require('./js/modules/Arc.module');
+const RECENT_ARCFILE_KEY = '662589eb-a07c-42eb-bac2-ddd9f75f7fc9';
 
 /**
  * @param {string} elementID
@@ -40,18 +37,12 @@ var switchView = function switchView(viewName) {
     var views = document.getElementsByClassName('view');
 
     for (var i = 0, n = views.length; i < n; ++i) {
-        views[i].style.visibility = 'hidden';
+        views[i].style.visibility = 'hidden'; // TODO display?
     }
 
     document.getElementById(viewName).style.visibility = 'visible';
 };
 
-// /**
-//  * @param {string} dialogName
-//  */
-// var showDialog = function showDialog(dialogName) {
-//     document.getElementById(dialogName).style.visibility = 'visible';
-// };
 
 /**
  * @param {string} viewName
@@ -63,23 +54,19 @@ var clearView = function clearView(viewName) {
 document.addEventListener('DOMContentLoaded', function () {
     var recentArcFile = localStorage.getItem(RECENT_ARCFILE_KEY);
 
-    //if (recentArcFile) {
-    //    setDirectory(recentArcFile);
-    //} else {
-    switchView('directory-request');
-    //}
+    if (recentArcFile) {
+        // TODO check existence
+        setArcFile(recentArcFile);
+    } else {
+        switchView('directory-request');
+    }
 
     let dropZone = document.getElementsByClassName('dropZone')[0];
     let dragdrop = new FileDragDrop(dropZone);
+
     dragdrop.subscribe('drop', function (e, file) {
         let p = file.path;
-
-        let dir = p.split(path.sep);
-        dir.pop();
-        // dir = dir.join(path.sep);
-
-        setDirectory(p);
-        //setDirectory(path.join(dir, path.basename(p).split('.')[0]));
+        setArcFile(p);
     });
 
     dropZone.addEventListener('click', function () {
@@ -90,7 +77,7 @@ document.addEventListener('DOMContentLoaded', function () {
 /**
  * @param {string} pathName
  */
-var setDirectory = function setDirectory(pathName) {
+var setArcFile = function setArcFile(pathName) {
     localStorage.setItem(RECENT_ARCFILE_KEY, pathName);
 
     switchView('arc-list');
@@ -108,23 +95,20 @@ var setDirectory = function setDirectory(pathName) {
             name: contentName
         });
     }
-    
-    // var arcfiles = document.getElementsByClassName('arcfile');
-    //
-    // for (i = 0, n = arcfiles.length; i < n; ++i) {
-    //     arcfiles[i].addEventListener('click', function () {
-    //         console.log(path.join(pathName, this.textContent), localStorage.getItem(RECENT_WORKSPACE_KEY))
-    //         dearcer(path.join(pathName, this.textContent), localStorage.getItem(RECENT_WORKSPACE_KEY));
-    //     });
-    // }
-    //
-    // var recentWorkspace = localStorage.getItem(RECENT_WORKSPACE_KEY);
-    //
-    // if (recentWorkspace) {
-    //     setWorkspace(recentWorkspace);
-    // } else {
-    //     showDialog('workspace-request');
-    // }
+
+    /**
+     * @type {HTMLButtonElement}
+     */
+    var close = document.createElement('button');
+
+    close.textContent = 'Закрыть';
+    close.className = 'close';
+
+    close.onclick = function() {
+        switchView('directory-request');
+    };
+
+    view.appendChild(close);
 };
 
 var openArcFile = function openDirectory() {
@@ -137,66 +121,9 @@ var openArcFile = function openDirectory() {
     });
 
     if (path.length) {
-        setDirectory(path[0]);
+        setArcFile(path[0]);
     }
 };
-
-// function closeDialog(dialogName) {
-//     document.getElementById(dialogName).style.visibility = 'hidden';
-// }
-//
-// /**
-//  * @param element
-//  * @param {string} html
-//  */
-// var appendHtml = function appendHtml(element, html) {
-//     var dummy = document.createElement('div');
-//     dummy.innerHTML = html;
-//
-//     while (dummy.children.length > 0) {
-//         element.appendChild(dummy.children[0]);
-//     }
-// };
-//
-// var updateWorkspace = function updateWorkspace(event) {
-//     if (event !== 'rename') {
-//         return;
-//     }
-//
-//     var workspace = document.getElementById('workspace');
-//     var workFolder = localStorage.getItem(RECENT_WORKSPACE_KEY);
-//     var directories = fs.readdirSync(workFolder);
-//
-//     workspace.innerHTML = null;
-//     for (var i = 0, n = directories.length; i < n; ++i) {
-//         var stats = fs.statSync(path.join(workFolder, directories[i]));
-//
-//         workspace.innerHTML += dots.workfolder({
-//             name: directories[i],
-//             isFile: stats.isFile()
-//         })
-//     }
-// };
-//
-// var setWorkspace = function setWorkspace(pathName) {
-//     localStorage.setItem(RECENT_WORKSPACE_KEY, pathName);
-//     closeDialog('workspace-request');
-//
-//     appendHtml(document.getElementById('arc-list'), dots.workspace());
-//
-//     fs.watch(pathName, updateWorkspace);
-//     updateWorkspace('rename', null);
-// };
-//
-// var openWorkspace = function openWorkspace() {
-//     var path = dialog.showOpenDialog({
-//         properties: ['openDirectory']
-//     });
-//
-//     if (path.length) {
-//         setWorkspace(path[0]);
-//     }
-// };
 
 var menu = Menu.buildFromTemplate([
     {
